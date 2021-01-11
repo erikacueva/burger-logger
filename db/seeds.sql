76268f11508c523902637774666a052a8a863ac7
erika cueva <ecueva88@gmail.com>
2021-01-10 18:54:10 -0800

    created connection.js file and db files.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Double NorthWestern Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Infamous Star');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Widowmaker', true);
+
